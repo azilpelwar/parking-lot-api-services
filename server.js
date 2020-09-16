0a5a53637a0aebcfb47668f3a6f57918c94c6b1d
ankur <ankur.zilpelwar@gmail.com>
2020-09-16 18:38:44 +0530

    added label for printing DB error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var db;
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
-    console.log(err);
+    console.log("Error connecting to DB",err);
     process.exit(1);
   }
 
